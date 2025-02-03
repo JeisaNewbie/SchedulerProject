ca7ba5d9b39a697cf0700f657966eed6ebe177ec
jhwang2 <ahkiler12@gmail.com>
2025-02-03 15:38:26 +0900

    DOCS: README.md 등록

--- sql/schedule.sql
+++ sql/schedule.sql
@@ -1,8 +1,6 @@
 -- 유저 테이블
 CREATE TABLE user (
                       id BIGINT PRIMARY KEY AUTO_INCREMENT,
-#     schedule_id BIGINT,
-#     FOREIGN KEY (schedule_id) REFERENCES schedule(id),
                       email VARCHAR(100) UNIQUE NOT NULL ,
                       password INT NOT NULL ,
                       name CHAR(10) UNICODE NOT NULL
@@ -13,8 +11,6 @@ CREATE TABLE to_do (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     user_id BIGINT,
     FOREIGN KEY (user_id) REFERENCES user(id),
-#     schedule_id BIGINT,
-#     FOREIGN KEY (schedule_id) REFERENCES schedule(id),
     date DATE,
     registered_date DATETIME,
     modified_date DATETIME,
